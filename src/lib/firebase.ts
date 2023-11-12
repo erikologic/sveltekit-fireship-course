d600672846170ef2dc54f87bb148028f88a2646f
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2023-11-12 12:48:02 +0000

    user store + logout

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,50 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAIXd28A3zNiroBd2LTr-RQckANtB2s-VE",
-    authDomain: "svelte-course-fireship-ceaf8.firebaseapp.com",
-    projectId: "svelte-course-fireship-ceaf8",
-    storageBucket: "svelte-course-fireship-ceaf8.appspot.com",
-    messagingSenderId: "1049472524342",
-    appId: "1:1049472524342:web:a42d340bb4c47ea91b3897",
-    measurementId: "G-RDHCQPL1F0"
-  };
+  apiKey: "AIzaSyAIXd28A3zNiroBd2LTr-RQckANtB2s-VE",
+  authDomain: "svelte-course-fireship-ceaf8.firebaseapp.com",
+  projectId: "svelte-course-fireship-ceaf8",
+  storageBucket: "svelte-course-fireship-ceaf8.appspot.com",
+  messagingSenderId: "1049472524342",
+  appId: "1:1049472524342:web:a42d340bb4c47ea91b3897",
+  measurementId: "G-RDHCQPL1F0",
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn("Auth is not initialized or not in browser");
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    };
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
